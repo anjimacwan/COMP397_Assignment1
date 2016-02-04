32c3d16266d66b46a18420308646af6616c20f06
anjimacwan <macwananji@gmail.com>
2016-02-04 00:52:41 -0500

    Re-run commit

--- Scripts/scenes/menu.js
+++ Scripts/scenes/menu.js
@@ -16,18 +16,18 @@ var scenes;
         // Start Method
         Menu.prototype.start = function () {
             // add the MENU label to the scene
-            var bg = PIXI.Sprite.fromImage("../../Assets/images/VampireOutcast.jpg");
-            bg.anchor.x = 0;
-            bg.anchor.y = 0;
-            bg.position.x = 50;
-            bg.position.y = 50;
+            /*var bg=PIXI.Sprite.fromImage("../../Assets/images/VampireOutcast.jpg");
+            bg.anchor.x=0;
+            bg.anchor.y=0;
+            bg.position.x=50;
+            bg.position.y=50; */
             this._gameLabel = new createjs.Text("Menu Scene", "60px Consolas", "#000000");
             this._gameLabel.regX = this._gameLabel.getMeasuredWidth() * 0.5;
             this._gameLabel.regY = this._gameLabel.getMeasuredHeight() * 0.5;
             this._gameLabel.x = config.Screen.CENTER_X;
             this._gameLabel.y = config.Screen.CENTER_Y;
             this.addChild(this._gameLabel);
-            this.addChild(bg);
+            //this.addChild(bg);
             // add the START button to the MENU scene
             this._startButton = new objects.Button("StartButton", config.Screen.CENTER_X, config.Screen.CENTER_Y + 60);
             this.addChild(this._startButton);

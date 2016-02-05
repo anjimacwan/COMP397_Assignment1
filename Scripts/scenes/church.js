ec09d7878b3874a14326e44421f490dd4f381a7d
anjimacwan <macwananji@gmail.com>
2016-02-05 14:57:49 -0500

    updated final state

--- Scripts/scenes/church.js
+++ Scripts/scenes/church.js
@@ -16,17 +16,27 @@ var scenes;
         // Start Method
         Church.prototype.start = function () {
             // add the PLAY label to the scene
-            this._overLabel = new createjs.Text("Bravo!!! You are awesome! \n\nYou defeated the blood-suckers!! \n\nCongrats!!!!!", "20px Consolas", "white");
+            this._overLabel = new createjs.Text("Bravo!!! You are awesome! \n\nYou defeated the blood-suckers!! \n\nCongrats!!!!! \n\nWanna play again?? Click the button below!", "20px Consolas", "white");
             this._overLabel.regX = 0;
             this._overLabel.regY = 0;
             this._overLabel.x = 0;
             this._overLabel.y = 0;
             this.addChild(this._overLabel);
+            // add the START button to the DARKFOREST scene
+            this._startButton = new objects.Button("StartButton", config.Screen.CENTER_X, config.Screen.CENTER_Y + 120);
+            this.addChild(this._startButton);
+            // NEXT Button event listener
+            this._startButton.on("click", this._startButtonClick, this);
             // add this scene to the global stage container
             stage.addChild(this);
         };
         // PLAY Scene updates here
         Church.prototype.update = function () {
+        };
+        Church.prototype._startButtonClick = function (event) {
+            // Switch to the OVER Scene
+            scene = config.Scene.MENU;
+            changeScene();
         };
         return Church;
     })(objects.Scene);
--- Scripts/scenes/church.ts
+++ Scripts/scenes/church.ts
@@ -3,6 +3,7 @@ module scenes {
     export class Church extends objects.Scene {
         //PRIVATE INSTANCE VARIABLES ++++++++++++
         private _overLabel: createjs.Text;
+        private _startButton:objects.Button;
        
         // CONSTRUCTOR ++++++++++++++++++++++
         constructor() {
@@ -15,14 +16,23 @@ module scenes {
         public start(): void {
 
             // add the PLAY label to the scene
-            this._overLabel = new createjs.Text("Bravo!!! You are awesome! \n\nYou defeated the blood-suckers!! \n\nCongrats!!!!!", "20px Consolas", "white");
+            this._overLabel = new createjs.Text("Bravo!!! You are awesome! \n\nYou defeated the blood-suckers!! \n\nCongrats!!!!! \n\nWanna play again?? Click the button below!", "20px Consolas", "white");
             this._overLabel.regX = 0;
             this._overLabel.regY = 0;
             this._overLabel.x = 0;
             this._overLabel.y = 0;
             this.addChild(this._overLabel);
-
-
+            
+            
+            // add the START button to the DARKFOREST scene
+            this._startButton = new objects.Button(
+                "StartButton",
+                config.Screen.CENTER_X,
+                config.Screen.CENTER_Y + 120);
+            this.addChild(this._startButton);
+           
+            // NEXT Button event listener
+            this._startButton.on("click", this._startButtonClick, this);
             // add this scene to the global stage container
             stage.addChild(this);
         }
@@ -30,6 +40,12 @@ module scenes {
         // PLAY Scene updates here
         public update(): void {
 
+        }
+        
+        private _startButtonClick(event: createjs.MouseEvent) {
+            // Switch to the OVER Scene
+            scene = config.Scene.MENU;
+            changeScene();
         }
     }
 }
